e83c07f351ba011b7f977066e5d85dc597ff1e7b
Jon Jagger <jon@jaggersoft.com>
2018-02-10 09:06:12 +0000

    [ci skip] improved version check

--- check_version.sh
+++ check_version.sh
@@ -2,7 +2,8 @@
 set -e
 
 readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2)
-readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/ubuntu-build-essential:16.04 sh -c 'cat /etc/issue')
+readonly IMAGE_NAME=cyberdojofoundation/ubuntu-build-essential:16.04
+readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c '. /etc/os-release && echo ${VERSION_ID}')
 
 if echo ${ACTUAL} | grep -q ${EXPECTED}; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
