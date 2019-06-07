112a08361ed73259230599889fd92ee820429f18
Jon Jagger <jon@jaggersoft.com>
2019-06-07 15:03:49 +0100

    fix check-version which relied in cwd

--- check_version.sh
+++ check_version.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly EXPECTED=$(cat ${MY_DIR}/README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
 readonly IMAGE_NAME=cyberdojofoundation/ubuntu-build-essential:16.04
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c '. /etc/os-release && echo ${VERSION_ID}')
 
-if echo ${ACTUAL} | grep -q ${EXPECTED}; then
+if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
 else
   echo "VERSION EXPECTED: ${EXPECTED}"
